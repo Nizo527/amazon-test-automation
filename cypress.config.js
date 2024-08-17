1eeacf93903e10dcdff916cb66f86d00899bc2e1
Alejandro Zamora <zamorahop@hotmail.com>
2024-08-16 20:36:12 -0500

    Implementación de video para las evidencias

--- cypress.config.js
+++ cypress.config.js
@@ -2,20 +2,23 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // Implementar los eventos de nodo aquí si es necesario
+    setupNodeEvents(on, config) {    
     },
-    // Configura la carpeta donde Cypress buscará los archivos de prueba
     specPattern: 'cypress/integration/**/*.{js,jsx,ts,tsx}',
-    // Define la URL base si es necesario
     baseUrl: 'https://www.rappi.com',
+
+    // Configuración para habilitar videos
+    video: true,
+    videoCompression: 32,
+    videosFolder: 'cypress/videos',
   },
-  
-  // Configuración del reporte
-  reporter: 'mochawesome',
-  reporterOptions: {
+
+    // Configuración del reporte
+    reporter: 'mochawesome',
+    reporterOptions: {
     reportDir: 'cypress/reports',
     reportFilename: 'report',
     timestamp: 'yyyy-mm-dd-HH-MM-ss',
   },
 });
+
